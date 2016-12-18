d1b1add565f4dc670058cbd9de18e4eea5d2f7d1
Daniel Stahl <danstahl1138@gmail.com>
2016-12-17 19:55:37 -0600

    made more general

--- CharacteristicFunctions.h
+++ CharacteristicFunctions.h
@@ -34,14 +34,14 @@ namespace chfunctions {
     }
 
     /**
-    Helper function to compute ODE series found in http://web.stanford.edu/~duffie/dps.pdf page 10
+    Helper function to compute ODE series found in http://web.stanford.edu/~duffie/dps.pdf page 10. THIS NEEDS TO BE MORE GENERIC!!!  Only a subset of duffie's ODEs
     @u complex value for phi(u)
     @cf characteristic function of the "jump" process
     @currentValues currentValues of the the system of ODEs
     @sigma volatility of diffusion
     @lambda 
     */
-    template<typename T>
+   /* template<typename T>
     std::vector<T > duffieODE(const auto& u, auto&& cf, const std::vector<T >& currentValues, double sigma, double lambda, double a, double delta, double b){ //double alpha, double mu, double beta, double c,
         auto sig=sigma*sigma*.5;
         return 
@@ -49,7 +49,32 @@ namespace chfunctions {
             currentValues[0]*currentValues[0]*sig+cf(u+currentValues[0]*delta)*lambda-lambda-currentValues[0]*a, 
             currentValues[0]*b*a
         };
+    }*/
+
+    /**Helper function to compute ODE series found in http://web.stanford.edu/~duffie/dps.pdf page 10. 
+    note that this is with respect to T-t not t so the equations have signs switched
+    */
+    template<typename T>
+    std::vector<T > duffieODE(const auto& u, const std::vector<T >& currentValues, const auto& rho0, const auto& rho1, const auto& K0, const auto& K1, const auto& H0, const auto& H1, const auto& l0, const auto& l1, auto&& cf){ //double alpha, double mu, double beta, double c,
+        //auto sig=sigma*sigma*.5;
+        auto cfPart=cf(u)-1.0;
+        auto sigPart=currentValues[0]*currentValues[0]*.5;
+        return //beta, alpha
+        {
+            -(rho1-K1*currentValues[0]-sigPart*H1-l1*cfPart),
+            -(rho0-K0*currentValues[0]-sigPart*H0-l0*cfPart)
+        };
     }
+    /**Helper function to compute ODE series found in http://web.stanford.edu/~duffie/dps.pdf page 10. 
+    note that this is with respect to T-t not t so the equations have signs switched
+    */
+    template<typename T>
+    std::vector<T > duffieODEStandard(const std::vector<T >& currentValues, const auto& rho0, const auto& rho1, const auto& K0, const auto& K1, const auto& H0, const auto& H1, const auto& l0, const auto& l1, auto&& cf){ //double alpha, double mu, double beta, double c,
+        //auto sig=sigma*sigma*.5;
+        return duffieODE(currentValues[0], currentValues, rho0, rho1, K0, K1, H0, H1, l0, l1, cf);
+    }
+
+
     /**
     Helper function to compute expontential of the Duffie ODE
     */
@@ -60,7 +85,7 @@ namespace chfunctions {
     /**
 
     */
-    template<typename T>
+    /*template<typename T>
     T jumpProcess(
         const T& u, 
         const auto& t, 
@@ -76,11 +101,24 @@ namespace chfunctions {
         return expAffine(
             rungekutta::computeFunctional(t, numODE, std::vector<T>({0, 0}),
                 [&](double t, const std::vector<T>& x){
-                    return duffieODE(u, cf, x, sigma, lambda, a, delta, b);
+                    //return duffieODE(u, cf, x, sigma, lambda, a, delta, b);
+                    return duffieODE(
+                        u+delta*x[0],//u
+                        x, //init values
+                        0.0, //rho0
+                        0.0, //rho1
+                        -a*b, //K0
+                        -a, //K1,
+                        0.0, //H0
+                        sigma*sigma, //H1, 
+                        0.0,//l0
+                        lambda, //l1
+                        cf
+                    );
                 }
             ),
             v0
         );
-    }
+    }*/
 }
 #endif
